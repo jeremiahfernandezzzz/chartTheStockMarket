01fb6ac474b2822c5ff798452afb006b567e9fcf
Glitch (pricey-lighter) <none>
2018-01-12 15:50:55 +0000

    🍣🏓 Checkpoint server.js:491517/27 public/views/stock.html:491517/202

--- server.js
+++ server.js
@@ -27,55 +27,6 @@ app.get("/dreams", function (request, response) {
   response.send(dreams);
 });
 
-
-app.get("/stock/:qwe", function (request, response) {
-  //var tickr = ["haha","hehe"];
-  
-  //console.log(tickr)
-  const year = 2018
-  const tickers = [];
-  request.params.qwe.split(",").forEach(function(element){
-    tickers.push(element)
-  });
-  //tickers.forEach(function(tick){
-  var tick = ["intc"]
-    Eod.config({ year, tick });
-    Eod.fetch()
-    .then((data) => {
-      Object.values(data[0]["dataset"]["data"]).forEach(function(element){
-        var dateArray = [];
-        
-        element[0].split("-").forEach(function(element){
-          dateArray.push(element)
-        }) ;
-        
-        function toMonth(month){
-            if (month == 1) {return "January"}
-            if (month == 2) {return "February"}
-            if (month == 3) {return "March"}
-            if (month == 4) {return "April"}
-            if (month == 5) {return "May"}
-            if (month == 6) {return "June"}
-            if (month == 7) {return "July"}
-            if (month == 8) {return "August"}
-            if (month == 9) {return "September"}
-            if (month == 10) {return "October"}
-            if (month == 11) {return "November"}
-            if (month == 12) {return "December"}
-        }
-        
-        dateArray[1] = toMonth(dateArray[1])
-        //console.log("year" + date[0] + " month" + date[1] + " date" + date[2])
-        var date = new Date(dateArray[1] + " " + dateArray[2] + " " + dateArray[0])
-        var unixTime = date.getTime();
-        element[0] = unixTime
-      }) //raw JSON response
-      //console.log(data[0]["dataset"]["data"])
-    })
-  //})
-  
-});
-
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
 app.post("/dreams", function (request, response) {
   dreams.push(request.query.dream);
