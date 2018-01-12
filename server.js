d904345a773c9a447fccab69cfa4f6af5d7572a6
Glitch (pricey-lighter) <none>
2018-01-12 07:59:43 +0000

    ✌️🌿 Checkpoint server.js:491517/781

--- server.js
+++ server.js
@@ -40,7 +40,30 @@ app.get("/stock/:qwe", function (request, response) {
     Eod.fetch()
     .then((data) => {
       Object.values(data[0]["dataset"]["data"]).forEach(function(element){
-        element[0] = 
+        var date = [];
+        
+        element[0].split("-").forEach(function(element){
+          date.push(element)
+        }) ;
+        
+        function toMonth(month){
+            if (month == 1) {return "January"}
+            if (month == 2) {return "February"}
+            if (month == 3) {return "March"}
+            if (month == 4) {return "April"}
+            if (month == 5) {return "May"}
+            if (month == 6) {return "June"}
+            if (month == 7) {return "July"}
+            if (month == 8) {return "August"}
+            if (month == 9) {return "September"}
+            if (month == 10) {return "October"}
+            if (month == 11) {return "November"}
+            if (month == 12) {return "December"}
+        }
+        
+        date[1] = toMonth(date[1])
+        console.log("year" + date[0] + " month" + date[1] + " date" + date[2])
+        
       }) //raw JSON response
     })
   })
