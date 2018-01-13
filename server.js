568748729677b914d0e66402dc42f20afb7be2fd
Glitch (pricey-lighter) <none>
2018-01-13 06:21:19 +0000

    🌼🎽 Checkpoint server.js:491517/294 public/views/stock.html:491517/87 socket.io.js:491517/6

--- server.js
+++ server.js
@@ -31,11 +31,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/public/views/stock.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-
 app.post("/stock", function (request, response) {
   MongoClient.connect(url, function(err, db){
         if (db){
@@ -47,19 +42,6 @@ app.post("/stock", function (request, response) {
         }
       })
 });
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
